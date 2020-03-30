cc0ea56e302e037bffb3ed9922db5886f624b9ee
Erasmo Magliozzi <magliozzierasmo98@gmail.com>
2020-03-30 16:32:52 +0200

    commit

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('guest.welcome');
 });
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::prefix('admin')
+      ->namespace('Admin')
+      ->middleware('auth')
+      ->group(function() {
+      Route::get('/', 'HomeController@index')
+      ->name('home');
+      });
